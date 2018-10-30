6f9561e7ad5f24ea88838de71a09c7edd08fa9b7
Jason Halsey <jason@halsey-design.com>
2018-10-30 14:21:43 -0700

    Add slider and video sections

--- js/tlc.js
+++ js/tlc.js
@@ -1,13 +1,4 @@
 jQuery(document).ready(function() {
-  var menuToggle = jQuery('#js-centered-navigation-mobile-menu').unbind();
-  jQuery('#js-centered-navigation-menu').removeClass("show");
-  
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    jQuery('#js-centered-navigation-menu').slideToggle(function(){
-      if(jQuery('#js-centered-navigation-menu').is(':hidden')) {
-        jQuery('#js-centered-navigation-menu').removeAttr('style');
-      }
-    });
-  });
-});
+  jQuery('.top_star').css('opacity', 1);
+  console.log('bullshit');
+}); // End doc.ready function
